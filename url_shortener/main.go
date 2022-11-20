ce30d341f0ecd43845d65e2d38ce78de1b3208a1
Kamee Kaze <k4m33k4z3@protonmail.com>
2022-11-20 22:41:16 +0100

    url redirects

--- url_shortener/main.go
+++ url_shortener/main.go
@@ -7,9 +7,38 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var (
+	pathsToUrls = map[string]string{
+		"dogs": "https://www.somesite.com/a-story-about-dogs",
+		"cats": "https://www.somesite.com/a-story-about-cats",
+	}
+)
+
 func main() {
 	r := mux.NewRouter()
+
+	// define routes
+	r.HandleFunc("/{url}", UrlHandler).Methods("GET")
+	r.NotFoundHandler = http.HandlerFunc(NotFoundHandler)
+
 	//start http server
 	fmt.Println("Running on http://localhost" + ":8080")
 	http.ListenAndServe(":8080", r)
+}
+
+func UrlHandler(w http.ResponseWriter, r *http.Request) {
+	// get url parameter
+	url := mux.Vars(r)["url"]
+	// check if url exists, if it does redirect to the path
+	if newUrl := pathsToUrls[url]; newUrl != "" {
+		http.Redirect(w, r, newUrl, http.StatusPermanentRedirect)
+	} else { // else 404 not found
+		NotFoundHandler(w, r)
+	}
+}
+
+// custom 404 handler
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("<h1>The requested URL was not found.</h1>\n"))
 }
