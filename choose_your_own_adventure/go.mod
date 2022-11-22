472e8f8124ccb7f7c1b14b5e4db9e0cf9827d847
Kamee Kaze <k4m33k4z3@protonmail.com>
2022-11-22 22:17:06 +0100

    render chapters in html

--- choose_your_own_adventure/go.mod
+++ choose_your_own_adventure/go.mod
@@ -1,3 +1,5 @@
 module github.com/KameeKaze/gophercises/choose_your_own_adventure
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0
--- choose_your_own_adventure/main.go
+++ choose_your_own_adventure/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
 type Chapter struct {
@@ -16,14 +20,42 @@ type Chapter struct {
 	} `json:"options"`
 }
 
+type News struct {
+	Title  string
+	Story  string
+	Option []string
+}
+
+var (
+	Story map[string]Chapter
+)
+
 func main() {
 	// read story file
 	file, err := os.ReadFile("gopher.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	story := ParseJSON([]byte(file))
-	fmt.Println(story)
+	Story = ParseJSON([]byte(file))
+
+	r := mux.NewRouter()
+
+	// define routes
+	r.HandleFunc("/{url}", StoryHandler).Methods("GET")
+
+	//start http server
+	fmt.Println("Running on http://localhost" + ":8080")
+	http.ListenAndServe(":8080", r)
+}
+
+func StoryHandler(w http.ResponseWriter, r *http.Request) {
+	url := mux.Vars(r)["url"]
+	tmplt, err := template.ParseFiles("templates/chapter.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	event := Story[url]
+	tmplt.Execute(w, event)
 }
 
 // parse json into struct
