44d86772adf7d84d28341ca1db16905cad591d9d
Kamee Kaze <k4m33k4z3@protonmail.com>
2022-11-24 21:22:14 +0100

    separate into functions

--- quiz/main.go
+++ quiz/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 	"time"
@@ -16,7 +16,6 @@ const (
 )
 
 var (
-	UserPoints  = 0
 	Filename    string
 	TimeoutFlag int
 )
@@ -27,44 +26,26 @@ func init() {
 	flag.Parse()
 }
 
+type Question struct {
+	Question string
+	Answer   string
+}
+
 func main() {
-	// open file
-	file, err := os.Open(Filename)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
+	quiz := createQuiz()
 
-	// read file into data
-	var data = make(map[string]string)
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		// split line by comma
-		line := strings.Split(scanner.Text(), ",")
-		// check if line has one comma
-		if len(line) != 2 {
-			panic("Invalid file content")
-		}
-		// add to data
-		data[line[0]] = line[1]
-	}
-
-	var answer string
 	// channel for timeout
-	timeout := make(chan int)
+	timeout := make(chan bool)
 	// always scan for answers
-	go func() {
-		for {
-			fmt.Println("Your answer:")
-			fmt.Scanln(&answer)
-			timeout <- 1
-		}
-	}()
+	var answer string
+	go scanAnswer(&answer, timeout)
+
 	// iterate over questions
-	for k, v := range data {
+	var userPoints int
+	for i := range quiz {
 		// print question
-		fmt.Println("Question:", k)
+		fmt.Println("Question:", quiz[i].Question)
+		fmt.Println("Your answer:")
 
 		if TimeoutFlag != 0 {
 			select {
@@ -72,20 +53,56 @@ func main() {
 			case <-time.After(time.Duration(TimeoutFlag) * time.Second):
 				fmt.Println(Red + "Timed out, next!" + Reset)
 			case <-timeout:
-				UserPoints += checkAnswer(answer, v)
+				userPoints += checkAnswer(answer, quiz[i].Answer)
 				// restet answer
 				answer = ""
 			}
 		} else {
 			<-timeout
-			UserPoints += checkAnswer(answer, v)
+			userPoints += checkAnswer(answer, quiz[i].Answer)
 			// restet answer
 			answer = ""
 		}
 		fmt.Println()
 	}
-	fmt.Printf("You scored %d points out of %d!\n", UserPoints, len(data))
+	fmt.Printf("You scored %d points out of %d!\n", UserPoints, len(quiz))
+}
 
+func scanAnswer(answer *string, timeout chan<- bool) {
+	for {
+		fmt.Scanln(answer)
+		timeout <- true
+	}
+}
+
+func readFile(filename string) []byte {
+	// read file
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return file
+}
+
+func createQuiz() (quiz []*Question) {
+	// read file
+	file := readFile(Filename)
+	// split file by new line
+	for _, line := range strings.Split(string(file), "\n") {
+		// split line by comma
+		line := strings.Split(string(line), ",")
+		// check if question and answer seperated by a comme
+		if len(line) != 2 {
+			log.Fatal("Invalid file content")
+			return
+		}
+		// append to quiz
+		quiz = append(quiz, &Question{
+			line[0],
+			line[1],
+		})
+	}
+	return quiz
 }
 
 func checkAnswer(answer, solution string) int {
