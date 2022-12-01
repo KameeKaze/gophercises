15b91ad281006c345f41309c6cb71d2add12f875
Kamee Kaze <k4m33k4z3@protonmail.com>
2022-12-01 21:09:05 +0100

    #9 shuffle deck

--- deck_of_cards/main.go
+++ deck_of_cards/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"math/rand"
+	"time"
+)
 
 var (
 	suits = []string{"♦", "♣", "♥", "♠"}
 	ranks = []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
-	deck  []Card
+	Deck  []Card
 )
 
 type Card struct {
@@ -13,13 +16,19 @@ type Card struct {
 	Rank string
 }
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 func main() {
 	for i := range suits {
 		for j := range ranks {
-			deck = append(deck, Card{suits[i], ranks[j]})
-			fmt.Println(suits[i], ranks[j])
+			Deck = append(Deck, Card{suits[i], ranks[j]})
 		}
-
 	}
+}
 
+func Shuffle(deck []Card) []Card {
+	rand.Shuffle(len(deck), func(i, j int) { deck[i], deck[j] = deck[j], deck[i] })
+	return deck
 }
